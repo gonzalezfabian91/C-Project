46b270af09269f06dd9e5f978572ff30c3068ccc
Fabian <gonzalezfabian91@gmail.com>
2020-06-22 16:51:54 -0700

    Project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,24 +5,110 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Casino.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 
 namespace Casino.Controllers
 {
     public class HomeController : Controller
     {
         private CasinoContext db;
+
+        private int? uid
+        {
+            get
+            {
+                return HttpContext.Session.GetInt32("UserId");
+            }
+        }
+
+        private bool isLoggedIn
+        {
+            get
+            {
+                return uid != null;
+            }
+        }
         public HomeController(CasinoContext context)
         {
             db = context;
         }
+
+        [HttpGet("")]
         public IActionResult Index()
         {
             return View("Index");
         }
 
-        public IActionResult Privacy()
+        [HttpPost("register")]
+        public IActionResult Register(User newUser)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                if (db.Users.Any(user => user.Email == newUser.Email))
+                {
+                    ModelState.AddModelError("Email", "Email is already in use");
+                    return View("Index");
+                }
+
+                PasswordHasher<User> hasher = new PasswordHasher<User>();
+                newUser.Password = hasher.HashPassword(newUser, newUser.Password);
+
+                db.Add(newUser);
+                db.SaveChanges();
+
+                HttpContext.Session.SetInt32("UserId", newUser.UserId);
+                return RedirectToAction("Dashboard", new{userId = newUser.UserId});
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                var userInDb = db.Users.FirstOrDefault(u => u.Email == login.LoginEmail);
+
+                if (userInDb == null)
+                {
+                    ModelState.AddModelError("LoginEmail", "Invalid Email or Password");
+                    return View("Index");
+                }
+
+                var hasher = new PasswordHasher<Login>();
+                var result = hasher.VerifyHashedPassword(login, userInDb.Password, login.LoginPassword);
+
+                if (result == 0)
+                {
+                    ModelState.AddModelError("LoginEmail", "Invalid Email or Password");
+                    return View("Index");
+                }
+
+                HttpContext.Session.SetInt32("UserId", userInDb.UserId);
+                return RedirectToAction("Dashboard", new {userId = userInDb.UserId});
+            }
+            return View("Index");
+        }
+
+        [HttpGet("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("dashboard")]
+        public IActionResult Dashboard()
+        {
+            if (!isLoggedIn)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Dashboard");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
