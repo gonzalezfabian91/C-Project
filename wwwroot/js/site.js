f49a7b14745f5e89dfbb1a25389ec310ec00ba3e
Bryce Alderete <brycealderete@gmail.com>
2020-06-24 16:31:02 -0700

    commit

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,8 +3,8 @@
 
 // Write your JavaScript code.
 $(document).ready(function(){
-    let stringval= ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"];
-    let suits = ["Heart","Diamond","Spade","Club"];
+    let stringval= ["Ace","2","3","4","5","6","7","8","9","10","Jack","c","Queen","King"];
+    let suits = ["Spade","Heart","Diamond","Club"];
 
     var deck;
     var dealerSevenHand;
@@ -12,16 +12,49 @@ $(document).ready(function(){
     
     function getDeck(){
         deck = [];
-        for(var i = 0; i < suits.length; i++)
-        {
-            let Val=1;
+        let frontspade ="&#127137;"
+        let frontheart ="&#127153;"
+        let frontdiamond ="&#127169;"
+        let frontclub ="&#127185;"
+        let Val=1;
+        
             for(var x = 0; x < stringval.length; x++)
             {
-                var card = {StringVal:stringval[x],Suit:suits[i], Value:Val};
+                
+                var card = {StringVal:stringval[x],Suit:suits[0], Value:Val, Back: "&#127136;", Front: frontspade};
                 deck.push(card);
-                Val++
+                frontspade = "&#1271"+(37+Val)+";"; 
+                Val++;
             }
-        }
+            deck.splice(11,1)
+            Val = 1;
+            for(var x = 0; x < stringval.length; x++)
+            {
+                var card = {StringVal:stringval[x],Suit:suits[1], Value:Val, Back: "&#127136;", Front: frontheart};
+                deck.push(card);
+                frontheart = "&#1271"+(53+Val)+";"; 
+                Val++;
+            }
+            deck.splice(24,1)
+            Val = 1;
+            for(var x = 0; x < stringval.length; x++)
+            {
+                var card = {StringVal:stringval[x],Suit:suits[2], Value:Val, Back: "&#127136;", Front: frontdiamond};
+                deck.push(card);
+                frontdiamond = "&#1271"+(69+Val)+";"; 
+                Val++;
+            }
+            deck.splice(37,1)
+            Val = 1;
+            for(var x = 0; x < stringval.length; x++)
+            {
+                var card = {StringVal:stringval[x],Suit:suits[3], Value:Val, Back: "&#127136;", Front: frontclub};
+                deck.push(card);
+                frontclub = "&#1271"+(85+Val)+";"; 
+                Val++;
+            }
+            deck.splice(50,1)
+        
         shuffle();
     }
     function clearsevenHands(){
@@ -50,7 +83,7 @@ $(document).ready(function(){
     
 }
             
-
+    
 
 
 
@@ -58,16 +91,25 @@ $(document).ready(function(){
         console.log("In 'DealAllCards' function");
         getDeck();
         clearsevenHands();
+        console.log(deck);
 
 
         for(var i=1;i<8;i++){
             let temp1=deal()
             let temp2=deal()
-            $("#playerseven"+i+"").html("<h5>"+temp1+"</h5>");
-            $("#dealerseven"+i+"").html('<img src="https://i.pinimg.com/originals/11/45/96/11459640e599fd105943f5a379d25248.jpg" alt="">');
+            $("#playerseven"+i+"").html('<h1>'+temp1.Front+'</h1>');
+            $("#dealerseven"+i+"").html('<h1>'+temp2.Back+'</h1>');
             dealerSevenHand+=temp2;
             playerSevenHand+=temp1;
-
+            
+        }
+        console.log("player cards");
+        for (let i = 0; i<playerSevenHand.length; i++){
+            console.log(`player card ${i+1}`, playerSevenHand);
+        }
+        console.log("Dealer cards");
+        for (let i = 0; i<playerSevenHand.length; i++){
+            console.log(`dealer card ${i+1}`, dealerSevenHand);
         }
         
         
